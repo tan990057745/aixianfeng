b929b8f3e084701fd586ede92d16ea02b3a8ea29
tan990057745 <990057745@qq.com>
2018-11-03 17:35:03 +0800

    爱鲜蜂首页静态页面完成

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from myapp.models import Wheel, Nav, Mustbuy
+from myapp.models import Wheel, Nav, Mustbuy, Shop, MainShow
 
 
 def index(request):
@@ -9,10 +9,25 @@ def index(request):
     navs = Nav.objects.all()
     mustbuys = Mustbuy.objects.all()
 
+    #商品展示部分
+    shopList = Shop.objects.all()
+    shophead = shopList[0]
+    shoptab = shopList[1:3]
+    shopclass = shopList[3:7]
+    shopcommend = shopList[7:11]
+
+    mainshows = MainShow.objects.all()
+
     data = {
         'wheels':wheels,
         'navs':navs,
         'mustbuys':mustbuys,
+
+        'shophead': shophead,
+        'shoptab': shoptab,
+        'shopclass': shopclass,
+        'shopcommend': shopcommend,
+        'mainshows': mainshows
     }
     return render(request,'index/index.html',context=data)
 
